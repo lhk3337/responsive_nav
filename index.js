ac6206ff64290a8e0c935b6f251fcba3b36d8472
HK <user@HK.local>
2020-05-13 17:19:35 +0900

    nav js modify

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const button = document.querySelector(".bars");
 const jsMenu = document.querySelector(".menu");
 const jsSns = document.querySelector(".sns");
+const jsBody = document.querySelector("body");
 button.addEventListener("click", function (event) {
   if (jsMenu.style.display == "none" && jsSns.style.display == "none") {
-    jsMenu.style.display = "inline";
-    jsSns.style.display = "inline";
+    jsMenu.style.display = "flex";
+    jsSns.style.display = "flex";
   } else {
     jsMenu.style.display = "none";
     jsSns.style.display = "none";
   }
 });
+
+function sizeScreen() {
+  const widthLength = window.innerWidth;
+  if (widthLength > 920) {
+    jsMenu.style.display = "flex";
+    jsSns.style.display = "flex";
+  }
+}
+window.addEventListener("resize", sizeScreen);
